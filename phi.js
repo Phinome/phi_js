a6b4aa228e1e326820dc76b3005663313245f3c7
Phinome <Long.phinome@gmail.com>
2013-05-20 15:47:10 +0800

    20130520 Commit
    Add functions

--- phi.js
+++ phi.js
@@ -50,3 +50,75 @@ function getHttpObject()
 }
 
 Hijax : 渐近增强地使用Ajax。使用JavaScript去拦截默认动作而不是一开始就使用JavaScript。
+
+function getNextElement( node )
+{
+	if( node.nodeType == 1 ) {
+		return node;
+	}
+	if( node.nextSibling ) {
+		return getNextElement( node.nextSibling );
+	}
+	return null;
+}
+
+function stripeTables( colors ) {
+	if( !document.getElementsByTagName)		return false;
+	var tables = document.getElementsByTagName( "table" );
+	var tablesLength = tables.length;
+
+	for(var i = 0; i <= tablesLength; i++) {
+		var odd = false;
+		var rows = document.getElementsByTagName( "tr" );
+		var rowsLength = rows.length;
+		for(var j = 0; j <= rowsLength; j++) {
+			if( odd == true ) {
+				rows[j].style.backgroundColor = colors;
+				odd = false;
+			} else
+			{
+				odd = true;
+			}
+		}
+	}
+}
+
+function heightLightRows () {
+	if( !document.getElementsByTagName )	return false;
+	var rows = document.getElementsByTagName( "tr" );
+	var rowsLength = rows.length;
+
+	for(var i = 0; i <= rowsLength; i++) {
+		rows[i].onmouseover = function () {
+			this.style.fontWeight = 'bold';
+		}
+		rows.onmouseout = function() {
+			this.style.fontWeight = 'normal';
+		}
+	}
+}
+
+function addClass ( element , value ) {
+	if( !element.className )
+	{
+		element.className = value;
+	} else
+	{
+		newClassName = element.className;
+		newClassName += " ";
+		newClassName += value;
+		element.className = newClassName;
+	}
+}
+
+function styleElementSibling ( tag , theClass ) {
+	if ( !document.getElementsByTagName)	return false;
+	var elems = document.getElementsByTagName( tag );
+	var elem;
+	var elemsLength = elems.length;
+
+	for( var i=0; i<= elemsLength; i++ ) {
+		elem = getNextElement( elems[i].nextSibling );
+		addClass( elem , theClass );
+	}
+}
